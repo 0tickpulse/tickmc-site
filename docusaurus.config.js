a36c8fb7f04c285399b2b9940a11fe55de238946
0tickpulse <62788361+0tickpulse@users.noreply.github.com>
2023-06-21 08:06:29 +0800

    fix some urls in docusaurus config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ const config = {
     ],
 
     // Set the production url of your site here
-    url: "https://your-docusaurus-test-site.com",
+    url: "https://www.tick-mc.net",
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: "/",
@@ -65,7 +65,7 @@ const config = {
                     sidebarPath: require.resolve("./sidebars.js"),
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    editUrl: "https://github.com/0tickpulse/tickmc-site/tree/main/",
                     remarkPlugins: [math],
                     rehypePlugins: [katex],
                 },
@@ -73,7 +73,7 @@ const config = {
                     showReadingTime: true,
                     // Please change this to your repo.
                     // Remove this to remove the "edit this page" links.
-                    editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+                    editUrl: "https://github.com/0tickpulse/tickmc-site/tree/main/",
                 },
                 theme: {
                     customCss: require.resolve("./src/css/custom.css"),
